e820a4f3d82d5e4c9c3c52a967db833399f7180e
Satwik Sinha <52771564+Satwik26@users.noreply.github.com>
2022-01-30 11:25:26 -0500

    Add files via upload

---
+++ mac_changer3.0.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import subprocess
+import optparse
+import re
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its Mac address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify an mac, use --help for more info")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] Changing Mac address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def current_mac_address(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("Could not find any Mac address in specified interface")
+    return mac_address_search_result
+
+
+change_mac(get_arguments().interface, get_arguments().new_mac)
+current_mac = current_mac_address(get_arguments().interface)
+if current_mac == get_arguments().new_mac:
+    print("[+] Mac address changed successfully " + current_mac)
+else:
+    print("[-] Mac address did not change")
